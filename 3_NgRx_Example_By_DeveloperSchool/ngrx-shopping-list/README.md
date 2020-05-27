d807c021cc75041b19214fb6339748844e47599f
anuragpatle <anurag.ap6@gmail.com>
2020-05-27 11:05:27 +0530

    P3: Add DevTools

--- 3_NgRx_Example_By_DeveloperSchool/ngrx-shopping-list/README.md
+++ 3_NgRx_Example_By_DeveloperSchool/ngrx-shopping-list/README.md
@@ -2,6 +2,13 @@
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 9.1.0.
 
+## Reference
+
+I copied this project from below links and the explaination of theis project is found here.
+https://developer.school/introduction-to-ngrx-store/
+https://developer.school/introduction-to-ngrx-effects/
+
+
 ## Development server
 
 Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
--- 3_NgRx_Example_By_DeveloperSchool/ngrx-shopping-list/src/app/app.module.ts
+++ 3_NgRx_Example_By_DeveloperSchool/ngrx-shopping-list/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { StoreModule } from '@ngrx/store';
 import { FormsModule } from '@angular/forms';
 import { ShoppingReducer } from './store/reducers/shopping.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -16,7 +18,8 @@ import { ShoppingReducer } from './store/reducers/shopping.reducer';
     FormsModule,
     StoreModule.forRoot({
       shopping: ShoppingReducer
-    })
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
